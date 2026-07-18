959907dbcecd6f4610c48494ea82c474b1f781a6
Amresh Verma <geniousamresh@gmail.com>
2026-07-18 16:12:58 +0530

    first commit

---
+++ src/test/java/com/retail/CloudGatewayServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.retail;
+/* package com.stroe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+ */
